a2d12aeaccecdf95539ceda4e450aea2489d1762
Alexandre Nakano <alex.nakano@gmail.com>
2021-05-15 18:44:35 -0300

    Select component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Radio from './components/radio';
 import Checkbox from './components/checkbox';
 import Input from './components/input';
 import CardSearch from './components/cardSearch';
+import Select from './components/select'
 
 import './style.scss';
 
@@ -11,30 +12,39 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Button transparent={false} text="Lorem Ipsum" />
-        <Button transparent={true} onButtonClicked={buttonTest} />
-        <div onChange={onChangeValue} >
-          <Radio name = 'teste' value='opcao1' defaultCheck={true}>
-          </Radio>
-          <Radio name = 'teste' value='opcao2'>
-          </Radio>
-        </div>
-        <Checkbox
-          value="Olar"
-          onCheckBoxClicked={getCheckboxValue}
-        >
-        </Checkbox>
-        <Input
-          placeholder='testettetete'
-          onValueInput={onValueInput}
-          maxLength='10'
-        >
-        </Input>
-        <CardSearch
-        >
-        </CardSearch>
-      </header>
+      <Button transparent={false} text="Lorem Ipsum" />
+      <br/>
+      <Button transparent={true} onButtonClicked={buttonTest} />
+      <br/>
+      <div onChange={onChangeValue} >
+        <Radio name = 'teste' value='opcao1' defaultCheck={true}>
+        </Radio>
+        <Radio name = 'teste' value='opcao2'>
+        </Radio>
+      </div>
+      <br/>
+      <Checkbox
+        value="Olar"
+        onCheckBoxClicked={getCheckboxValue}
+      >
+      </Checkbox>
+      <br/>
+      <Input
+        placeholder='testettetete'
+        onValueInput={onValueInput}
+        maxLength='10'
+      >
+      </Input>
+      <br/>
+
+      <CardSearch>
+      </CardSearch>
+      <br/>
+      <Select
+        options= {['Test F01', 'Test F02', 'Test F03', 'Test F4']}
+        onSelectedValue = {(e) => console.log(e)}
+      >
+      </Select>
     </div>
   );
 }
