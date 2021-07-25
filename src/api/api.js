89d2c75a066ca3391beab700d36677eb8ee67642
Alexandre Nakano <alex.nakano@gmail.com>
2021-07-25 14:44:39 -0300

    Change name variable.

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_DOMAIN || 'http://google.com';
+const url = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 
 export const api = axios.create({
 	baseURL: url
